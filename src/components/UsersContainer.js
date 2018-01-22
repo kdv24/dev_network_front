e5e7f0d9fa04f672271be583b6dcdee05e019697
Kelly de Vries <kdv@me.com>
2018-01-22 12:56:19 -0800

    add ajax call to rails app

--- src/components/UsersContainer.js
+++ src/components/UsersContainer.js
@@ -1,9 +1,27 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import '../App.css';
 
 class UsersContainer extends Component {
+
+  constructor(props) {
+  super(props)
+  this.state = {
+    users: []
+  }
+}
+
+componentDidMount() {
+  fetch('http://localhost:3001/api/v1/users.json')
+  .then(response => {
+    console.log(response)
+    this.setState({users: response.data})
+  })
+  .catch(error => console.log(error))
+}
+
   render() {
     return (
-      <div>
+      <div className="userlist">
         Users
       </div>
     )
