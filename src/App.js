2eedec7d7950c73966f02053e92b1accd7359b15
Kelly de Vries <kdv@me.com>
2018-01-22 11:58:03 -0800

    add initial view

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import UsersContainer from './components/UsersContainer';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">The Developer Network</h1>
+        </header>
+        <p className="App-intro">
+          By Developers.  For Developers.
+        </p>
+        <UsersContainer />
+      </div>
+    );
+  }
+}
+
+export default App;
